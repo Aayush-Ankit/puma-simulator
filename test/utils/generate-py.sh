3ea05d4ddfbdb83dd85c1326460ae8a35b087c55
yliborio <37150537+yliborio@users.noreply.github.com>
2020-02-27 08:50:41 -0300

    Local path removed
    Co-Authored-By: Plinio Silveira <plinio89s@gmail.com>

--- test/utils/generate-py.sh
+++ test/utils/generate-py.sh
@@ -1,7 +1,7 @@
 # Copy this file to compiler test folder where the .npy files are generated.
 # Update SIMULATOR_PATH value and execute it.
 
-SIMULATOR_PATH="/home/marte/Desktop/git_fork/puma-simulator" # simulator root path
+SIMULATOR_PATH=" " # simulator root path
 
 if [[ $SIMULATOR_PATH == "" ]] ; then
     print "Error, missing simulator path."
